300be2ae70b62d7e95a3f6f930ca31d37abaf6f0
Bill <william.augustine@myyahoo.com>
2023-11-20 09:15:46 -0600

    Added folders

--- Scripts/map1.gd
+++ Scripts/map1.gd
@@ -12,5 +12,5 @@ func _process(delta):
 
 
 func _on_area_2d_body_entered(body):
-	get_tree().change_scene_to_file("res://dungeon1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/dungeon1.tscn")
 	print(body)
